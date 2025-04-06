4c49b605d9f6c717ffef84313e840a7157534c92
VahosDev <juandavidvahosbohorquez4@gmail.com>
2025-04-06 17:48:53 -0500

    cambios

--- TechStore/js/techstore.js
+++ TechStore/js/techstore.js
@@ -4,6 +4,17 @@ function addProducts() {
   AllProducts.forEach((product) => {
     const articleProduct = document.createElement("article");
     articleProduct.classList = "product";
-    articleProduct.innerHTML = ``;
+    articleProduct.innerHTML = `
+    <h4 class="name">${product.name}</h4>
+          <img src="${product.image}" alt="${product.name}" />
+          <h3 class="price">${product.price}</h3>
+          <p class="description">
+            ${product.description}
+          </p>
+    `;
+    const article = document.querySelector(".product");
+    sectionOfProducts.appendChild(articleProduct);
   });
 }
+
+addProducts();
